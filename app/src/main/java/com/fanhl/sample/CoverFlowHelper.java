eae67fbffe6c1efd4087072bb05977056caf52af
fanhl <fanhonglin@klicen.com>
2017-02-15 15:36:48 +0800

    fixbug

--- app/src/main/java/com/fanhl/sample/CoverFlowHelper.java
+++ app/src/main/java/com/fanhl/sample/CoverFlowHelper.java
@@ -1,6 +1,7 @@
 package com.fanhl.sample;
 
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -9,9 +10,9 @@ import android.view.View;
  * Created by fanhl on 2017/2/15.
  */
 public class CoverFlowHelper {
-
+    public static final String TAG = CoverFlowHelper.class.getSimpleName();
     public static final float POSITION_THRESHOLD = 0.4f;
-    public static final float MAX_SCALE = 1.4f;
+    public static final float MAX_SCALE = 1f;
     private final ViewPager viewPager;
 
     public static CoverFlowHelper setup(ViewPager viewPager) {
@@ -26,11 +27,14 @@ public class CoverFlowHelper {
     private void init() {
         viewPager.setOffscreenPageLimit(4);
         //左右缩进合适的值
-        viewPager.setPageMargin(-viewPager.getContext().getResources().getDisplayMetrics().widthPixels / 3);
+        int marginPixels = -viewPager.getContext().getResources().getDisplayMetrics().widthPixels / 2;
+        Log.d(TAG, "marginPixels:" + marginPixels);
+        viewPager.setPageMargin(marginPixels);
         //中间大，两边大小渐变的动画变化，最大1.4倍
         viewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
             @Override public void transformPage(View page, float position) {
-                if (position < 1) {
+                Log.d(TAG, "page:" + page + " position:" + position);
+                if (position < -1) {
                     //This page is way off-screen to the left
                 } else if (position <= 1) {
                     if (position < 0) {
