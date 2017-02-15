f759020b2d672653fd5f0dd200e0cdf01e74fc7d
fanhl <fanhonglin@klicen.com>
2017-02-15 11:21:03 +0800

    add ViewPager

--- app/src/main/java/com/fanhl/sample/MainActivity.java
+++ app/src/main/java/com/fanhl/sample/MainActivity.java
@@ -1,13 +1,21 @@
 package com.fanhl.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.support.v4.view.ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        assignViews();
+    }
+
+    private void assignViews() {
+        this.viewPager = (ViewPager) findViewById(R.id.viewPager);
     }
 }
